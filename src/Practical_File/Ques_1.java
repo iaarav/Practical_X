75694f6dfdd9725bca991bf0c66e7932786db1e7
iaarav <aarav2008bansal@gmail.com>
2023-09-17 11:58:12 +0530

    First Commit

---
+++ src/Practical_File/Ques_1.java
@@ -0,0 +1,9 @@
+package Practical_File;
+
+//  Write a program in Java to solve the following series:
+//    S = 1- 11+111-1111+11111 ........15 terms
+public class Ques_1 {
+    public static void main(String[] args) {
+
+    }
+}
